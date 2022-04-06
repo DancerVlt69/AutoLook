00ad9735b9c7a7a25dd6cfbb86dd69f08aed5418
alRex-U <83890990+alrex-u@users.noreply.github.com>
2022-04-06 17:01:21 +0900

    improve InformationHUD

--- src/main/java/com/alrexu/autolook/render/hud/InformationHUD.java
+++ src/main/java/com/alrexu/autolook/render/hud/InformationHUD.java
@@ -11,12 +11,15 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
 import java.util.ArrayList;
 
 public class InformationHUD extends AbstractGui {
+	ResourceLocation BACKGROUND = new ResourceLocation("minecraft", "textures/gui/demo_background.png");
 	public float animationFunction(float phase) {
 		return 1 - (phase - 1) * (phase - 1);
 	}
@@ -35,7 +38,8 @@ public class InformationHUD extends AbstractGui {
 
 		Entity entity = lockOn.getTargetEntity();
 		if (entity != null) {
-			content.add(String.format("%s : §3%s", I18n.get("autolook.text.name"), entity.getDisplayName().getString()));
+			ITextComponent customName = entity.getCustomName();
+			content.add(String.format("%s : §1%s", I18n.get("autolook.text.name"), (customName != null ? customName : entity.getDisplayName()).getString()));
 		}
 		if (entity instanceof LivingEntity) {
 			LivingEntity living = (LivingEntity) entity;
@@ -45,21 +49,28 @@ public class InformationHUD extends AbstractGui {
 			ItemEntity item = (ItemEntity) entity;
 			content.add(String.format("%s : §2%4d", I18n.get("autolook.text.item.size"), item.getItem().getCount()));
 		}
-		content.add(String.format("%s : §b%7f", I18n.get("autolook.text.pos.x"), pos.x()));
-		content.add(String.format("%s : §b%7f", I18n.get("autolook.text.pos.y"), pos.y()));
-		content.add(String.format("%s : §b%7f", I18n.get("autolook.text.pos.z"), pos.z()));
+		content.add(String.format("%s : §1%7f", I18n.get("autolook.text.pos.x"), pos.x()));
+		content.add(String.format("%s : §1%7f", I18n.get("autolook.text.pos.y"), pos.y()));
+		content.add(String.format("%s : §1%7f", I18n.get("autolook.text.pos.z"), pos.z()));
 
 		final int boxWidth = Math.max(width / 4, 10 + content.stream().map(font::width).max(Integer::compareTo).orElse(0));
 		final int left;
 		if (aim.getTick() < 5) {
 			left = (int) (width - boxWidth * animationFunction((aim.getTick() + event.getPartialTicks()) / 5));
 		} else left = (int) width - boxWidth;
-		final int top = (int) (height * 0.25);
+		final int top = (int) (height * 0.10);
 		final int boxHeight = (font.lineHeight + 1) * content.size() + 10/*10 is margin(5 + 5)*/;
 
-		fill(stack, left, top, width, top + boxHeight, 0x99333333);
+		mc.textureManager.bind(BACKGROUND);
+		blit(stack, left, top, 0, 0, boxWidth, 5, 256, 256);
+		blit(stack, left, top + 5 + (font.lineHeight + 1) * content.size(), 0, 161, boxWidth, 5, 256, 256);
 		for (int i = 0; i < content.size(); i++) {
-			font.draw(stack, content.get(i), left + 5, top + 5 + (font.lineHeight + 1) * i, 0xFFFFFF);
+			final int lineTop = top + 5 + (font.lineHeight + 1) * i;
+			blit(stack, left, lineTop, 0, 5, boxWidth, font.lineHeight + 1, 256, 256);
+		}
+		for (int i = 0; i < content.size(); i++) {
+			final int lineTop = top + 5 + (font.lineHeight + 1) * i;
+			font.draw(stack, content.get(i), left + 5, lineTop, 0x333333);
 		}
 	}
 }
